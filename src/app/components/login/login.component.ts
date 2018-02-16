68bc95224d68e6818da6050aaad4962143e11694
Luis <luis_mh@outlook.es>
2018-02-15 18:10:48 -0600

    Pagina de login, y sevicio de login

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,23 +1,38 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute, Params} from '@angular/router';
 
+//los (..) es para salir de una carpeta, saimos de la carpeta login, luego de la carpeta components y entramos en la carpeta services donde tenemos nuestros servicios
+import { UserService } from '../../services/users/user.service'; 
+
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
+  providers: [UserService]
 })
 export class LoginComponent implements OnInit {
   public title: string;
+  public user;
 
   constructor(
-   // private _route: ActivatedRoute,
-   // private _router: Router
+    private _route: ActivatedRoute,
+    private _router: Router,
+    private _userService: UserService
   ) {
-    this.title = 'Componente de login';
+    this.title = 'Identificate';
+    this.user = {
+      "email":"",
+      "password":""
+    }
    }
 
   ngOnInit() {
     console.log('El componente de login ha sido cargado!!')
   }
 
+  onSubmit(){
+    console.log(this.user);
+    console.log(this._userService.signup());
+  }
 }
